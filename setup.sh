0605f6ff9b679733679342064902e123973cdac5
orest-gulman <orest.gulman@gmail.com>
2019-12-16 16:33:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,36 +2,36 @@
   col='\033[0;36m' # Cyan
   nocol='\033[0m' # No Color
   
-  echo echo "${col}Installing docker... ${nocol}"
+  echo -e "${col}Installing docker... ${nocol}"
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
   sleep 2
   
-  echo echo "${col}Installing docker-compose... ${nocol}"
+  echo -e "${col}Installing docker-compose... ${nocol}"
   apt-get install docker-compose -y
   sleep 2
 
-  echo "${col}Enter admin password for elasticsearch and kibana ${nocol}"
+  echo -e "${col}Enter admin password for elasticsearch and kibana ${nocol}"
   read -p ": " admin_pass
   
-  echo "${col}Running temp elasticsearch container for generating bcrypt hashes${nocol}"
+  echo - e "${col}Running temp elasticsearch container for generating bcrypt hashes${nocol}"
   docker run -d --name hash  -e "ES_JAVA_OPTS=-Xms4096m -Xmx4096m" -e "discovery.type=single-node" amazon/opendistro-for-elasticsearch:1.2.1
   sleep 10
   
   hash=$(docker exec -it hash /bin/bash -c "chmod 755 plugins/opendistro_security/tools/hash.sh; plugins/opendistro_security/tools/hash.sh -p $admin_pass")
-  echo "${col}Generated hash for admin password: $hash${nocol}"
+  echo -e "${col}Generated hash for admin password: $hash${nocol}"
   sleep 2
   sed -i -e "s/replacehash/$hash/g" internal_users.yml
   sleep 2
-  echo "${col}Removing temporary elasticsearch container!${nocol}"
+  echo -e "${col}Removing temporary elasticsearch container!${nocol}"
   docker rm hash -f
   sleep 2
-  echo "${col}Running elasticsearch and kibana containers${nocol}"
+  echo -e "${col}Running elasticsearch and kibana containers${nocol}"
   docker-compose up -d
   sleep 2
   docker ps
   valhost=$(hostname)
-  echo "${col}kibana is running http://$valhost:5601${nocol}"
-  echo "${col}elasticsearch is running http://$valhost:9200${nocol}"
-  echo "${col}admin password: $admin_pass${nocol}"
-  echo "${col}Now waiting...${nocol}"
+  echo -e "${col}kibana is running http://$valhost:5601${nocol}"
+  echo -e "${col}elasticsearch is running http://$valhost:9200${nocol}"
+  echo -e "${col}admin password: $admin_pass${nocol}"
+  echo -e "${col}Now waiting...${nocol}"
