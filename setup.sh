3cb5601f80f5cba5c2a895aca73adc02d89e3a8d
orest-gulman <orest.gulman@gmail.com>
2019-12-17 12:05:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,5 +51,5 @@
   
   echo "${col}Waitiong for elasticsearch and kibana up and running!${nocol}"
   sleep 120
-  echo "${col}Importing savad data for metricbeats!${nocol}"
-  curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@metrics2.ndjson -u admin:${admin_pass} -w "\n"
+  #echo "${col}Importing savad data for metricbeats!${nocol}"
+  #curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@metrics.ndjson -u admin:${admin_pass} -w "\n"
