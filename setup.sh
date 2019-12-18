674219c7e91236ae32d36cb0b9fa4ef24adf3c59
orest-gulman <orest.gulman@gmail.com>
2019-12-18 11:15:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,48 +22,44 @@ sleep 2
 apt install ruby -y
 sleep 1
 
+echo -e "${cyan}Enter admin password for elasticsearch and kibana ${nocol}"
+read -p ": " admin_pass
+
+echo -e "${cyan}Generating bcrypt hash for admin user...${nocol}"
+sleep 2
+admin_hash=$(htpasswd -bnBC 10 "" $admin_pass | tr -d ':\n')
+echo $admin_hash
+
 echo -e "${cyan}Generating password for kibanaserver user...${nocol}"
 sleep 2
 kibanaserver_pass=$(openssl rand -base64 10)
 echo $kibanaserver_pass
-echo -e "${cyan}Generating hash for kibanaserver user...${nocol}"
+echo -e "${cyan}Generating bcrypt hash for kibanaserver user...${nocol}"
 sleep 2
 kibanaserver_hash=$(htpasswd -bnBC 10 "" $kibanaserver_pass | tr -d ':\n')
 echo $kibanaserver_hash
-
-echo -e "${cyan}Enter admin password for elasticsearch and kibana ${nocol}"
-read -p ": " admin_pass
-
-echo -e "${cyan}Generating hash for admin user...${nocol}"
-sleep 2
-admin_hash=$(htpasswd -bnBC 10 "" $admin_pass | tr -d ':\n')
-echo $admin_hash
   
 #sed -i -e "s/replacehash/"$hash"/g" internal_users.yml
-echo "  hash: "${admin_hash}"" >> internal_users.yml
-sleep 2
+#echo "  hash: "${admin_hash}"" >> internal_users.yml
+#sleep 2
   
 echo -e "${cyan}Running elasticsearch and kibana containers${nocol}"
 docker-compose up -d
   
-echo -e "${cyan}Waiting 60sec...${nocol}"
-sleep 60
-docker ps
+echo -e "${cyan}Waiting 90sec...${nocol}"
+sleep 90
 
 echo -e "${cyan}Checking elasticsearch and kibana containers status...${nocol}"
+sleep 2
 if [ $(docker inspect -f '{{.State.Running}}' odfe-node1) == true ] && [ $(docker inspect -f '{{.State.Running}}' odfe-kibana) == true ]
 then
   echo -e "${cyan}Elasticsearch and kibana containers up...${nocol}"
+  docker ps
+  sleep 2
 else
  echo -e "${red}Docker containers are't running!...exit${nocol}"
- sleep 2
  exit 1
 fi
-  
-valhost=$(hostname)
-echo -e "${cyan}Kibana started http://$valhost:5601${nocol}"
-echo -e "${cyan}Elasticsearch started http://$valhost:9200${nocol}"
-echo -e "${cyan}admin password: $admin_pass${nocol}"
 
 echo -e "${cyan}Checking kibana ready status...${nocol}"
 sleep 2
@@ -92,7 +88,12 @@ for ((n=0;n<20;n++))
         break
     fi
 done
-
+  
+valhost=$(hostname)
+echo -e "${cyan}Kibana is running http://$valhost:5601${nocol}"
+echo -e "${cyan}Elasticsearch is running http://$valhost:9200${nocol}"
+echo -e "${cyan}admin user password: $admin_pass${nocol}"
+echo -e "${cyan}kibanaserver user password: $kibanaserver_pass${nocol}"
 #clear
 
 #echo "${cyan}Importing savad data for metricbeats!${nocol}"
