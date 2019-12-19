20d46635fb80d482e2b095746ae4b88e515ad95c
orest-gulman <orest.gulman@gmail.com>
2019-12-19 14:58:41 +0200

    Update config.rb

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'yaml'
 data = YAML.load_file('internal_users.yml')
 data['admin']['hash'] = ARGV[0]
 data['kibanaserver']['hash'] = ARGV[1]
-data['elasticsearch']['hash'] = ARGV[1]
+data['elasticsearch']['hash'] = ARGV[2]
 File.open("internal_users.yml", 'w') { |f| YAML.dump(data, f) }
 
 data = YAML.load_file('kibana.yml')
